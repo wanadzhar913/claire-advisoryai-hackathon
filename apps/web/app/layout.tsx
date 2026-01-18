750a116eff2adb367fac46b17bbd2401185182fd
wanadzhar913 <adzhar.faiq@gmail.com>
2026-01-19 01:53:32 +0800

    fix: attempt #2

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -31,8 +31,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+  
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={publishableKey}>
       <html lang="en" suppressHydrationWarning>
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <ThemeProvider
